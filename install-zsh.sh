fd8c864b76f02cc5274b9b66bce99645517fc239
Cade Daniel <edacih@gmail.com>
2019-01-10 17:36:12 +0000

    adding default editor configuration

--- install-zsh.sh
+++ install-zsh.sh
@@ -2,3 +2,4 @@
 # need to install zsh itself from your favorite package manager.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i 's/robbyrussell/gianu/g' ~/.zshrc
+echo "export EDITOR=vim" >> ~/.zshrc
