d944b297d5340e6ee7fac8f2ff151676496b71fa
Cade Daniel <edacih@gmail.com>
2019-01-17 23:37:01 +0000

    xterm export zsh

--- install-zsh.sh
+++ install-zsh.sh
@@ -3,3 +3,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i 's/robbyrussell/gianu/g' ~/.zshrc
 echo "export EDITOR=vim" >> ~/.zshrc
+
+#https://unix.stackexchange.com/questions/179173/make-less-highlight-search-patterns-instead-of-italicizing-them
+echo "export TERM=xterm-256color" >> ~/.zshrc
