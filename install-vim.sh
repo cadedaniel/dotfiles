f02a2e81b46c7a949d889d4fee6ba1c5edcb2623
Ubuntu <ubuntu@ip-172-31-76-18.ec2.internal>
2020-03-19 19:14:18 +0000

    Commenting out slow go plugin and removing hard dependency on ssh-add

--- install-vim.sh
+++ install-vim.sh
@@ -2,14 +2,14 @@
 
 set -e 
 
-eval `ssh-agent` && ssh-add ~/.ssh/edac_ih_gmail_com.private
-
 mkdir -p ~/.vim/bundle ~/.vim/autoload
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone git@github.com:majutsushi/tagbar.git ~/.vim/bundle/tagbar
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/rhysd/vim-color-spring-night ~/.vim/bundle/vim-color-spring-night
-git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+
+# TODO use modified version of plugin that doesn't slow down vim with compiler.
+#git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 git clone https://github.com/tpope/vim-pathogen ~/.vim/bundle/vim-pathogen
 
 ln -s ~/.vim/bundle/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
